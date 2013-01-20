bfae1fe98dcad2d13b068b980335e24a003b51d1
dkuehlwein <daniel.kuehlwein@gmail.com>
2013-01-21 00:22:10 +0100

    basic stuff

--- TeamDJ/hello.py
+++ TeamDJ/hello.py
@@ -6,3 +6,24 @@ Created on 21.01.2013
 
 if __name__ == '__main__':
     print 'hello world'
+    
+    # I am a one-line comment
+    
+    """
+    I am a multi-line comment
+    """
+    
+    # A simple for loop
+    print range(10)
+    for x in range(10):
+        print x
+    
+    # A boolean variable
+    janinaIsPretty = True
+    
+    
+    # A basic if-then-else construct
+    if janinaIsPretty:
+        print 'Janina is very pretty indeed!'
+    else:
+        print 'Janina is not very pretty.'
